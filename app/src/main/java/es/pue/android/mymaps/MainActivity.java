25c1443210b36ac1f61719a0adf4e5b0bedc9b65
azarrias <adolfo.zarrias@gmail.com>
2018-10-23 20:04:52 +0200

    Add main activity with show map implicit intent

--- app/src/main/java/es/pue/android/mymaps/MainActivity.java
+++ app/src/main/java/es/pue/android/mymaps/MainActivity.java
@@ -1,13 +1,39 @@
 package es.pue.android.mymaps;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnDisplayMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDisplayMap = (Button)findViewById(R.id.btnDisplayMap);
+
+        btnDisplayMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayMap();
+            }
+        });
+    }
+
+    private void displayMap(){
+        double lat = 41.4920189;
+        double lon = 2.3513412;
+
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:" + lat + "," + lon));
+
+        startActivity(i);
     }
 }
