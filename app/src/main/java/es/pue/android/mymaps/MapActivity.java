5115e2319b6b4fa690982553e9732302b35d9ed3
azarrias <adolfo.zarrias@gmail.com>
2018-10-25 19:09:16 +0200

    Create listener for list

--- app/src/main/java/es/pue/android/mymaps/MapActivity.java
+++ app/src/main/java/es/pue/android/mymaps/MapActivity.java
@@ -6,9 +6,11 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,6 +47,16 @@ public class MapActivity extends AppCompatActivity {
         cities.add("Lucerna");
 
         ListView citiesView = (ListView)findViewById((R.id.lstCitites));
+
+        AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MapActivity.this, "Ciudad:" + cities.get(position), Toast.LENGTH_LONG).show();
+            }
+        };
+
+        citiesView.setOnItemClickListener(listener);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
                 cities);
